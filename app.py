a125b1c1c27312b325d8610c2aaa8c32f60e8c77
Massimo Cristofori <cristoforimassimo@gmail.com>
2025-08-21 11:41:53 +0200

    Forzo deploy con modifica visibile

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def salva_json(path, dati):
     try:
         with open(path, "w", encoding="utf-8") as f:
             json.dump(dati, f, ensure_ascii=False, indent=4)
-        print(f"✅ File {path} salvato correttamente.")
+        print("✅ Dizionario Reggiano avviato correttamente.")
     except Exception as e:
         print(f"❌ Errore nel salvataggio di {path}: {e}")
 
